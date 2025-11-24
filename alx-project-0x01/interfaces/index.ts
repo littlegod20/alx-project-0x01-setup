bfdf74d300487c2399fed65dfba6edd539daaf0b
Theophilus Frimpong <theophilusfrimpong17@gmail.com>
2025-11-24 01:27:02 +0000

    Add PostModal component for creating new posts; update PostData and PostModalProps interfaces, and integrate modal into Posts page for enhanced post management.

--- alx-project-0x01/interfaces/index.ts
+++ alx-project-0x01/interfaces/index.ts
@@ -5,6 +5,18 @@ export interface PostProps {
   body: string;
 }
 
+export interface PostData {
+  userId: number;
+  id?: number;
+  title: string;
+  body: string;
+}
+
+export interface PostModalProps {
+  onClose: () => void;
+  onSubmit: (post: PostData) => void;
+}
+
 export interface Geo {
   lat: string;
   lng: string;
